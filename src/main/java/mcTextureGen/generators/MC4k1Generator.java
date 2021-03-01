747ce0021a0199c1829c542fda2383163015327a
Ned Loynd <41816363+NeRdTheNed@users.noreply.github.com>
2021-03-01 17:39:52 +1100

    Improve speed of code at expense of readability

--- src/main/java/mcTextureGen/generators/MC4k1Generator.java
+++ src/main/java/mcTextureGen/generators/MC4k1Generator.java
@@ -1,11 +1,16 @@
 package mcTextureGen.generators;
 
 import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferInt;
+import java.awt.image.DataBufferByte;
 
 import mcTextureGen.data.TextureGroup;
 
 public class MC4k1Generator implements TextureGenerator {
+
+    private static final int maxLightLevel = 2;
+    private static final int tileSize = 16;
+    private static final int unsignedByteMax = 0xFF;
+    private static final float unsignedByteMaxAsFloat = 0xFF;
 
     @Override
     public String getGeneratorName() {
@@ -19,38 +24,15 @@ public class MC4k1Generator implements TextureGenerator {
 
     /** Minecraft 4k-1's "XOR fractal" texture generation, for each light level */
     public TextureGroup xorTextures() {
-        int maxLightLevel = 2;
-        int tileWidth = 16;
-        int tileHeight = 16;
-        int andThing = 0xFF;
-        int slashThing = 0xFF;
-        BufferedImage[] xorImages = new BufferedImage[maxLightLevel + 1];
+        final BufferedImage[] xorImages = new BufferedImage[maxLightLevel + 1];
 
         for (int tileLightLevel = 0; tileLightLevel <= maxLightLevel; tileLightLevel++) {
-            BufferedImage tile = new BufferedImage(tileWidth, tileHeight, 1);
-            int[] tileIntData = ((DataBufferInt) tile.getRaster().getDataBuffer()).getData();
+            final BufferedImage tile = new BufferedImage(tileSize, tileSize, BufferedImage.TYPE_BYTE_GRAY);
+            final byte[] tileIntData = ((DataBufferByte) tile.getRaster().getDataBuffer()).getData();
 
-            for (int tileX = 0; tileX < tileWidth; tileX++) {
-                for (int tileY = 0; tileY < tileHeight; tileY++) {
-                    float tileLightModifier = 1.0F;
-                    tileLightModifier *= 1.0F - (tileLightLevel * 0.2F);
-                    int brightness = 0xFF;
-                    brightness = (int) (tileLightModifier * 0xFF);
-
-                    if (brightness < 0) {
-                        brightness = 0;
-                    }
-
-                    if (brightness > 0xFF) {
-                        brightness = 0xFF;
-                    }
-
-                    int tileRBG = ((tileX ^ tileY) * 8) + 128;
-                    tileRBG |= (tileRBG << 16) | (tileRBG << 8);
-                    int tileRed = (((tileRBG >> 16) & andThing) * brightness) / slashThing;
-                    int tileGreen = (((tileRBG >> 8) & andThing) * brightness) / slashThing;
-                    int tileBlue = ((tileRBG & andThing) * brightness) / slashThing;
-                    tileIntData[(tileX * tileWidth) + tileY] = (tileRed << 16) | (tileGreen << 8) | tileBlue;
+            for (int tileX = 0; tileX < tileSize; tileX++) {
+                for (int tileY = 0; tileY < tileSize; tileY++) {
+                    tileIntData[(tileX * tileSize) + tileY] = (byte) ((((((tileX ^ tileY) * 8) + 128) & unsignedByteMax) * (int) ((1.0F - (tileLightLevel * 0.2F)) * unsignedByteMaxAsFloat)) / unsignedByteMax);
                 }
             }
 
