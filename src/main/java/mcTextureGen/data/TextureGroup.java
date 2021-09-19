ae60117a9526b394c1da206bb648f6fa88dc0b0a
Ned Loynd <41816363+NeRdTheNed@users.noreply.github.com>
2021-09-20 00:04:41 +1000

    Delegate helper constructor to main constructor

--- src/main/java/mcTextureGen/data/TextureGroup.java
+++ src/main/java/mcTextureGen/data/TextureGroup.java
@@ -8,8 +8,7 @@ public final class TextureGroup {
     public final RenderedImage[] textureImages;
 
     public TextureGroup(final String textureName, final RenderedImage textureImage) {
-        textureGroupName = textureName;
-        textureImages = new RenderedImage[] { textureImage };
+        this(textureName, new RenderedImage[] { textureImage });
     }
 
     public TextureGroup(final String textureName, final RenderedImage[] textureImages) {
